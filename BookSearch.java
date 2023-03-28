032b450397cebd7bc06816dcd467a1b28da6c740
willi348 <willi348@sheridancollege.ca>
2023-03-28 13:00:23 -0400

    Added "Add New Book" feature, made window non-resizeable, Added Refresh Feature,

--- BookSearch.java
+++ BookSearch.java
@@ -1,5 +1,4 @@
 package LibrarySystem;
-import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -13,7 +12,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,40 +23,66 @@ public class BookSearch extends Application {
     Pane pane = new Pane();
     TextField txtSearch = new TextField("Search");
     Button BTSearch = new Button("Search");
+    Button BTAdd = new Button("Add");
+    Button BTRefresh = new Button("Refresh");
+    Button BTSave = new Button("Save & Quit");
     
-    File file = new File("BackupInventory.txt");
+    File file = new File("Inventory.txt");
     Library inv = new Library(file);
-    Text result = new Text(setText(inv));
+    Text result = new Text(inv.setText());
     SearchButton search = new SearchButton(txtSearch, result, inv.getInv());
     
     
     pane.setPadding(new Insets(10, 10, 10, 10));
     txtSearch.setOnAction(search);
     BTSearch.setOnAction(search);
+    BTAdd.setOnAction(e -> {
+        Stage newStage = createNewStage(inv);
+        newStage.show();
+    });
+    BTRefresh.setOnAction(e -> {
+            result.setText(inv.setText());
+    });
+    BTSave.setOnAction(e ->{
+        try {
+            inv.exportInventory(file);
+            primaryStage.close();
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+    });
+    BTAdd.setTranslateY(50);
+    BTAdd.setTranslateX(0);
+    BTRefresh.setTranslateX(50);
+    BTRefresh.setTranslateY(50);
+    BTSave.setTranslateY(470);
     ScrollBar scroll = new ScrollBar();
     Text title = new Text("Book Search System");
     scroll.setOrientation(Orientation.VERTICAL);
     title.setTranslateY(10);
     scroll.setTranslateX(475);
+    scroll.setTranslateY(100);
+    scroll.setScaleY(3);
+    scroll.setMax(500);
     result.setX(200);
     result.setY(10);
 
     scroll.setOnMouseClicked(e -> {
         result.setTranslateY(scroll.getValue() * -1);
     });
+      scroll.setOnScroll(e -> {
+          result.setTranslateY(scroll.getValue() * -1);
+      });
     txtSearch.setTranslateY(20);
     BTSearch.setTranslateY(20);
     BTSearch.setTranslateX(100);
-    pane.getChildren().add(title);
-    pane.getChildren().add(scroll);
-    pane.getChildren().add(txtSearch);
-    pane.getChildren().add(BTSearch);
-    pane.getChildren().add(result);
+    pane.getChildren().addAll(title,scroll,txtSearch,BTSearch,result,BTAdd,BTRefresh,BTSave);
 
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 500, 500);
     primaryStage.setTitle("Book Search & Storage System"); // Set the stage title
     primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.setResizable(false);
     primaryStage.show(); // Display the stage
   }
   
@@ -66,12 +90,53 @@ public class BookSearch extends Application {
     launch(args);
   }
 
-  public static String setText(Library inv){
-    String result = "";
-    for(int i = 0; i < inv.inventory.size(); i++) {
-			result+= inv.getInv().get(i).toString() + "\n";
-		}	
-    return result;
+
+  public static Stage createNewStage(Library inv){
+      Stage secondStage = new Stage();
+      Pane pane = new Pane();
+
+      TextField txtTitle = new TextField("Title");
+      TextField txtVolume = new TextField("Volume");
+      TextField txtAuthor = new TextField("Author");
+      TextField txtGenre = new TextField("Genre");
+      TextField txtShelfNumber = new TextField("Shelf Number");
+      Button BTSubmit = new Button("Submit");
+
+      BTSubmit.setOnAction(e -> {
+              inv.getInv().add(new Book(txtTitle.getText(), txtVolume.getText(), txtAuthor.getText(), txtGenre.getText(), txtShelfNumber.getText()));
+              secondStage.close();
+      });
+      txtTitle.setTranslateY(10);
+      txtTitle.setTranslateX(10);
+
+      txtVolume.setTranslateY(10);
+      txtVolume.setTranslateX(160);
+
+      txtAuthor.setTranslateY(50);
+      txtAuthor.setTranslateX(10);
+
+      txtGenre.setTranslateY(50);
+      txtGenre.setTranslateX(160);
+
+      txtShelfNumber.setTranslateY(90);
+      txtShelfNumber.setTranslateX(10);
+
+      BTSubmit.setTranslateX(10);
+      BTSubmit.setTranslateY(150);
+
+      pane.getChildren().add(txtTitle);
+      pane.getChildren().add(txtVolume);
+      pane.getChildren().add(txtAuthor);
+      pane.getChildren().add(txtGenre);
+      pane.getChildren().add(txtShelfNumber);
+      pane.getChildren().add(BTSubmit);
+
+      Scene scene = new Scene(pane, 500, 500);
+      secondStage.setTitle("Add Book to System"); // Set the stage title
+      secondStage.setScene(scene); // Place the scene in the stage
+      return secondStage;
+
+
   }
 }
 
--- Library.java
+++ Library.java
@@ -7,12 +7,14 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Library {
-	ArrayList<Book> inventory = new ArrayList<Book>();
+	private ArrayList<Book> inventory = new ArrayList<Book>();
+	private File myFile;
 	
 	
 	public Library() {}
 	
 	public Library(File myFile) throws FileNotFoundException {
+		this.myFile = myFile;
 		setInventory(myFile);
 	}
 	
@@ -36,48 +38,38 @@ public class Library {
 		System.out.println(inventory.get(i));
 	}
 	
-	public void exportinventory(File myFile) throws FileNotFoundException {
-		PrintWriter outputfile = new PrintWriter(myFile);
+	public void exportInventory(File myFile) throws FileNotFoundException {
+		PrintWriter outputFile = new PrintWriter(myFile);
 		for(int i = 0; i < inventory.size(); i++) {
 			String str = inventory.get(i).textFormat();
-			outputfile.println(str);
+			outputFile.println(str);
 		}
-		outputfile.close();
-	}
-	
-	public ArrayList<Book> search(String criteria, String field) {
-		ArrayList<String> compare = new ArrayList<String>();
-		ArrayList<Book> hits = new ArrayList<Book>();
-		System.out.println("switching " + field.toLowerCase() + " and " + criteria.toLowerCase());
-		switch(field.toLowerCase()) {
-		case "author":
-			inventory.forEach((b) -> compare.add(b.getAuthor()));
-			break;
-		case "volume number":
-			inventory.forEach((b) -> compare.add(b.getVolumeNumber()));
-			break;
-		case "title":
-			inventory.forEach((b) -> compare.add(b.getTitle()));
-			break;
-		case "genre":
-			inventory.forEach((b) -> compare.add(b.getGenre()));
-			break;
-		case "shelf number":
-			inventory.forEach((b) -> compare.add(b.getShelfNumber()));
-			break;
-		}
-		for(int i = 0; i < compare.size(); i++) {
-			String thing = compare.get(i).toLowerCase();
-			System.out.println("Comparing " + thing + " and " + criteria.toLowerCase() + " ::: are equal?: " + thing.equals(criteria.toLowerCase()));
-			if(thing.equals(criteria.toLowerCase())) {
-				System.out.println("adding " + thing);
-				hits.add(inventory.get(i));
-			}
-		}
-		return hits;
+		outputFile.close();
 	}
 
 	public ArrayList<Book> getInv(){
 		return inventory;
+	}
+
+	public void addNew(String title, String volumeNumber, String author, String genre, String shelfNumber)throws FileNotFoundException{
+		PrintWriter outputFile = new PrintWriter(myFile);
+		String str = new Book(title, volumeNumber, author, genre, shelfNumber).textFormat();
+		outputFile.println(addBookText() + str);
+		outputFile.close();
+	}
+	public String setText(){
+		String result = "";
+		for(int i = 0; i < inventory.size(); i++) {
+			result+= inventory.get(i).toString() + "\n";
+		}
+		return result;
+	}
+
+	private String addBookText(){
+		String result = "";
+		for(int i = 0; i < inventory.size(); i++) {
+			result+= inventory.get(i).textFormat() + "\n";
+		}
+		return result;
 	}
 }

9fe2425f9d94b1d235f6639185a73cb8652fce0a
willi348 <willi348@sheridancollege.ca>
2023-03-30 13:41:29 -0400

    Added Edit, and Delete features

--- BookSearch.java
+++ BookSearch.java
@@ -25,15 +25,16 @@ public class BookSearch extends Application {
 
       // Created Panes
     StackPane pane = new StackPane();
-    ScrollPane pane2 = new ScrollPane();
-    Pane pane3 = new Pane();
-    Pane pane4 = new Pane();
+    ScrollPane scrollPane = new ScrollPane();
+    Pane titlePane = new Pane();
+    Pane buttonPane = new Pane();
 
     // Created Nodes to place within Panes
     Text title = new Text("Book Search System");
     TextField txtSearch = new TextField("Search");
     Button BTSearch = new Button("Search");
     Button BTAdd = new Button("Add");
+    Button BTEdit = new Button("Edit");
     Button BTSave = new Button("Save & Quit");
     File file = new File("Inventory.txt");
     Library inv = new Library(file);
@@ -44,8 +45,12 @@ public class BookSearch extends Application {
     txtSearch.setOnAction(e -> search(txtSearch, result, inv.getInv())); // Allows you to press enter after typing to search
     BTSearch.setOnAction(e -> search(txtSearch, result, inv.getInv())); // Allows you to press the search button when done typing
     BTAdd.setOnAction(e -> {
-        Stage newStage = createNewStage(inv, result); // Creates a new stage with the features defined in the createNewStage class
+        Stage newStage = createAddStage(inv, result); // Creates a new stage with the features defined in the createNewStage class
         newStage.show(); // Displays the Stage
+    });
+    BTEdit.setOnAction(e -> {
+        Stage newStage = createEditStage(inv, result);
+        newStage.show();
     });
     BTSave.setOnAction(e ->{
         try {
@@ -57,26 +62,29 @@ public class BookSearch extends Application {
     });
     // set locations of the nodes within the panes
     BTAdd.setTranslateY(50);
-    BTAdd.setTranslateX(57);
+    BTAdd.setTranslateX(63);
     BTSave.setTranslateY(460);
+    BTSearch.setTranslateY(50);
+    BTSearch.setTranslateX(0);
+    BTEdit.setTranslateX(113);
+    BTEdit.setTranslateY(50);
     title.setTranslateY(10);
     result.setX(200);
     result.setY(10);
     txtSearch.setTranslateY(20);
-    BTSearch.setTranslateY(50);
-    BTSearch.setTranslateX(0);
+
 
     // set locations and parameters of the Panes
-    pane2.setTranslateX(100);
-    pane2.setFitToWidth(true);
-    pane2.setFitToHeight(true);
-    pane2.setMaxWidth(300);
-    pane2.setContent(result);
+    scrollPane.setTranslateX(100);
+    scrollPane.setFitToWidth(true);
+    scrollPane.setFitToHeight(true);
+    scrollPane.setMaxWidth(300);
+    scrollPane.setContent(result);
 
     // Add Nodes to their respective panes
-    pane3.getChildren().addAll(title);
-    pane4.getChildren().addAll(txtSearch,BTSearch, BTAdd,BTSave);
-    pane.getChildren().addAll(pane2, pane3, pane4);
+    titlePane.getChildren().addAll(title);
+    buttonPane.getChildren().addAll(txtSearch,BTSearch, BTAdd,BTSave,BTEdit);
+    pane.getChildren().addAll(titlePane, buttonPane, scrollPane);
 
     // Create a scene and place it in the stage
     Scene scene = new Scene(pane, 500, 500);
@@ -91,10 +99,11 @@ public class BookSearch extends Application {
   }
 
 // Creates and returns a new Stage consisting of 5 Test fields and a Button to add a new book to ArrayList
-  public static Stage createNewStage(Library inv, Text result){
+  public static Stage createAddStage(Library inv, Text result){
       Stage secondStage = new Stage(); // Creates a new scene
       Pane pane = new Pane(); // Creates a new Pane
       // Creates 5 TextField nodes and the submit button
+
       TextField txtTitle = new TextField("Title");
       TextField txtVolume = new TextField("Volume");
       TextField txtAuthor = new TextField("Author");
@@ -129,6 +138,8 @@ public class BookSearch extends Application {
       BTSubmit.setTranslateX(10);
       BTSubmit.setTranslateY(150);
 
+
+
       // Adds nodes to the pane
       pane.getChildren().addAll(txtTitle,txtVolume,txtAuthor,txtGenre,txtShelfNumber,BTSubmit);
 
@@ -138,6 +149,121 @@ public class BookSearch extends Application {
       secondStage.setScene(scene); // Place the scene in the stage
       return secondStage;
   }
+
+    public static Stage createEditStage(Library inv, Text result){
+        Stage secondStage = new Stage(); // Creates a new scene
+        StackPane pane = new StackPane(); // Creates a new Pane
+        Pane textPane = new Pane();
+        Pane textPane2 = new Pane();
+        ScrollPane scrollPane = new ScrollPane();
+
+        String stringText = "";
+
+        for(int i = 0; i < inv.getInv().size(); i++) {
+            stringText+= "Index " + i + ": " + inv.getInv().get(i).toString() + "\n";
+        }
+        String resultCopy = stringText;
+
+        Text text = new Text(stringText);
+        // Creates 5 TextField nodes and the submit button
+        TextField txtIndex = new TextField("Index");
+        Button BTSubmit = new Button("Submit");
+
+        TextField txtTitle = new TextField("Title");
+        TextField txtVolume = new TextField("Volume");
+        TextField txtAuthor = new TextField("Author");
+        TextField txtGenre = new TextField("Genre");
+        TextField txtShelfNumber = new TextField("Shelf Number");
+        Button BTSubmit2 = new Button("Submit");
+        Button BTDel = new Button("Delete");
+
+
+
+        // Submit button action
+        BTSubmit.setOnAction(e -> {
+            if(txtIndex.getText().equals("")){
+                text.setText(resultCopy);
+            }else {
+                text.setText(inv.getInv().get(Integer.valueOf(txtIndex.getText())).toString());
+            }
+            BTSubmit.setVisible(false);
+            txtIndex.setVisible(false);
+            pane.getChildren().add(textPane2);
+
+        });
+        txtIndex.setOnAction(e -> {
+            if(txtIndex.getText().equals("")){
+                text.setText(resultCopy);
+            }else {
+                text.setText(inv.getInv().get(Integer.valueOf(txtIndex.getText())).toString());
+            }
+            BTSubmit.setVisible(false);
+            txtIndex.setVisible(false);
+            pane.getChildren().add(textPane2);
+        });
+
+        BTSubmit2.setOnAction(e -> {
+            inv.getInv().get(Integer.parseInt(txtIndex.getText())).setAuthor(txtAuthor.getText());
+            inv.getInv().get(Integer.parseInt(txtIndex.getText())).setTitle(txtTitle.getText());
+            inv.getInv().get(Integer.parseInt(txtIndex.getText())).setGenre(txtGenre.getText());
+            inv.getInv().get(Integer.parseInt(txtIndex.getText())).setShelfNumber(txtShelfNumber.getText());
+            inv.getInv().get(Integer.parseInt(txtIndex.getText())).setVolumeNumber(txtVolume.getText());
+
+            secondStage.close(); // Closes the new Stage
+            result.setText(inv.setText()); // Redraws the list of books
+        });
+
+        BTDel.setOnAction(e -> {
+            inv.getInv().remove(Integer.parseInt(txtIndex.getText()));
+
+            secondStage.close(); // Closes the new Stage
+            result.setText(inv.setText()); // Redraws the list of books
+        });
+        // Sets the locations of the nodes
+        txtIndex.setTranslateY(10);
+        txtIndex.setTranslateX(10);
+
+        BTSubmit.setTranslateX(10);
+        BTSubmit.setTranslateY(90);
+
+        txtTitle.setTranslateY(10);
+        txtTitle.setTranslateX(10);
+
+        txtVolume.setTranslateY(50);
+        txtVolume.setTranslateX(10);
+
+        txtAuthor.setTranslateY(90);
+        txtAuthor.setTranslateX(10);
+
+        txtGenre.setTranslateY(130);
+        txtGenre.setTranslateX(10);
+
+        txtShelfNumber.setTranslateY(170);
+        txtShelfNumber.setTranslateX(10);
+
+        BTSubmit2.setTranslateX(10);
+        BTSubmit2.setTranslateY(210);
+        BTDel.setTranslateX(70);
+        BTDel.setTranslateY(210);
+
+        scrollPane.setTranslateX(100);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setMaxWidth(300);
+        scrollPane.setContent(text);
+
+        // Adds nodes to the pane
+        textPane.getChildren().addAll(txtIndex,BTSubmit);
+        textPane2.getChildren().addAll(txtTitle, txtVolume, txtAuthor, txtGenre, txtShelfNumber, BTSubmit2, BTDel);
+
+        pane.getChildren().addAll(textPane, scrollPane);
+
+        Scene scene = new Scene(pane, 500, 500); // Creates a new scene with the pane in it, and sets its size
+        secondStage.setResizable(false);
+        secondStage.setTitle("Edit a Book in the System"); // Set the stage title
+        secondStage.setScene(scene); // Place the scene in the stage
+        return secondStage;
+    }
 
   // Search Method for finding books based on a String
   public static void search(TextField txtSearch, Text result, ArrayList<Book> list){
